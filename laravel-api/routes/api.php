d478b405aff586dc8f65f7ebc6d6c164a7f380b1
Enrico Colombo <enrico.colombo@gmail.com>
2020-11-16 19:08:33 +0100

    Created Api authentication Controller

--- laravel-api/routes/api.php
+++ laravel-api/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,15 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+// Route for login API
+Route::post('login', [AuthController::class, 'login']);
+// Route for register API
+Route::post('register', [AuthController::class, 'register']);
+
+// Protected routes
+Route::middleware('auth:api')->group(function(){
+    // Route for details user API
+    Route::post('details', [AuthController::class, 'user_info']);
 });
